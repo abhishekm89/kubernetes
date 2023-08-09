acc4df5a04b6febf94e0132bb90934da3e7fd917
abhishekm89 <magaji.abhishek@gmail.com>
2023-08-09 13:39:27 +0530

    pre-requisites

--- commands_steps.sh
+++ commands_steps.sh
@@ -1,4 +1,4 @@
-# Creating EBS Volume for DB Instance
+# Creating EBS Volume for MySQL DB Instance
 aws ec2 create-volume --availability-zone=us-east-1a --size=3 --volume-type=gp2
 # output:
 {
@@ -20,3 +20,4 @@ kubeclt get nodes
 kubectl describe node i-0647a5d1137fde9d6 | grep us-east
 kubectl label node i-0647a5d1137fde9d6 zone=us-east-1a
 kubectl label node i-0162a4689c5193d71 zone=us-east-1b
+
