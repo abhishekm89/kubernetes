6e64b5b4f4c68594d424f6a7f1439a34a301e7d0
abhishekm89 <magaji.abhishek@gmail.com>
2023-08-09 16:00:45 +0530

    updated key_names

--- commands_steps.sh
+++ commands_steps.sh
@@ -1,4 +1,4 @@
-# Creating EBS Volume for MySQL DB Instance
+# Creating EBS Volume for MySQL DB Instance:
 aws ec2 create-volume --availability-zone=us-east-1a --size=3 --volume-type=gp2
 # output:
 {
@@ -15,9 +15,23 @@ aws ec2 create-volume --availability-zone=us-east-1a --size=3 --volume-type=gp2
     "MultiAttachEnabled": false
 }
 
-# Labeling WorkerNodes
+# Edit Tags on Volume (vol-0e8758ca1b8288234) via CLI/ManagementConsole
+Key: KubernetesCluster
+Value: kops.abhishekmagaji.in
+
+# Labeling WorkerNodes:
 kubeclt get nodes
-kubectl describe node i-0647a5d1137fde9d6 | grep us-east
+kubectl describe node i-0647a5d1137fde9d6 | grep us-east-1
 kubectl label node i-0647a5d1137fde9d6 zone=us-east-1a
 kubectl label node i-0162a4689c5193d71 zone=us-east-1b
+
+# Deployment on Clusters:
+kubectl create -f app-secret.yml
+kubectl create -f db-deploy.yml
+kubectl create -f db-clusterIP.yml
+kubectl create -f memcached-deploy.yml
+kubectl create -f memcached-clusterIP.yml
+kubectl create -f rabbitmq-deploy.yml
+kubectl create -f rabbitmq-clusterIP.yml
+
 
